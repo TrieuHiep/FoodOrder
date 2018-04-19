7de0115e58e0873ca2d00bd263689f4052ccec7c
unknown <dinhmaichi5296@gmail.com>
2018-04-20 00:16:20 +0700

    add

--- app/src/main/java/com/foodorder/tatsuya/foodorder/activity/LoginActivity.java
+++ app/src/main/java/com/foodorder/tatsuya/foodorder/activity/LoginActivity.java
@@ -9,6 +9,15 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.foodorder.tatsuya.foodorder.R;
 import com.foodorder.tatsuya.foodorder.UserSession;
 import com.foodorder.tatsuya.foodorder.model.personpkg.Account;
@@ -18,14 +27,18 @@ import com.foodorder.tatsuya.foodorder.task.OnTaskCompleted;
 public class LoginActivity extends AppCompatActivity implements OnTaskCompleted<Boolean> {
 
     private Button btnLogin;
+    private LoginButton loginButton;
     private TextView tvRegister, tvFacebook;
     private EditText edtUsername, edtPassword;
+    private CallbackManager callbackManager;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
         init();
     }
 
@@ -34,7 +47,28 @@ public class LoginActivity extends AppCompatActivity implements OnTaskCompleted<
         btnLogin = findViewById(R.id.btnLogin);
         edtUsername = findViewById(R.id.edtUsername);
         edtPassword = findViewById(R.id.edtPassword);
-        tvFacebook = findViewById(R.id.tv_facebook);
+        loginButton = (LoginButton) findViewById(R.id.tv_facebook);
+
+        callbackManager = CallbackManager.Factory.create();
+
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Toast.makeText(LoginActivity.this, "onSuccess", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(LoginActivity.this, "onCancel", Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Toast.makeText(LoginActivity.this, "onError", Toast.LENGTH_SHORT).show();
+
+            }
+        });
 
 
         tvRegister.setOnClickListener(new View.OnClickListener() {
@@ -70,6 +104,12 @@ public class LoginActivity extends AppCompatActivity implements OnTaskCompleted<
     }
 
     @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
     public void handle(Boolean value) {
         if (value) {
             Toast.makeText(LoginActivity.this, "Login successfully!", Toast.LENGTH_SHORT).show();
@@ -81,7 +121,17 @@ public class LoginActivity extends AppCompatActivity implements OnTaskCompleted<
             Intent intent = new Intent(LoginActivity.this, MainActivity.class);
             super.startActivity(intent);
         } else {
+            YoYo.with(Techniques.Shake)
+                    .duration(300)
+                    .repeat(5)
+                    .playOn(findViewById(R.id.edt_username));
+            YoYo.with(Techniques.Shake)
+                    .duration(300)
+                    .repeat(5)
+                    .playOn(findViewById(R.id.edt_password));
+
             Toast.makeText(LoginActivity.this, "Login failed!", Toast.LENGTH_SHORT).show();
+
         }
     }
 }
